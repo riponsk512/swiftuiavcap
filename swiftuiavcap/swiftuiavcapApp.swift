508046833339cb4ecbb4a08537cb8a6b4a2161a2
Ripon Seikh <34369083+riponsk512@users.noreply.github.com>
2023-07-15 21:37:30 +0530

    Initial Commit

---
+++ swiftuiavcap/swiftuiavcapApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuiavcapApp.swift
+//  swiftuiavcap
+//
+//  Created by Ripon sk on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftuiavcapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
